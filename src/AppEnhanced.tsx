79038495af8663e9e57c2e17290a8a3766d12cd9
陈浩（Hao Chen） <3418085891@qq.com>
2026-06-15 10:50:53 +0800

    Add enhanced app shell with burst overlay

---
+++ src/AppEnhanced.tsx
@@ -0,0 +1,11 @@
+import App from './App';
+import CenterBurstOverlay from './components/CenterBurstOverlay';
+
+export default function AppEnhanced() {
+  return (
+    <>
+      <App />
+      <CenterBurstOverlay />
+    </>
+  );
+}
