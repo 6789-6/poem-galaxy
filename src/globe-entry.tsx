4bce7133f248047dd2f73a4cb7102349f9b7781f
陈浩（Hao Chen） <3418085891@qq.com>
2026-06-15 22:30:44 +0800

    Load premium poetry globe UI skin

--- src/globe-entry.tsx
+++ src/globe-entry.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { PoetryGlobeApp } from './PoetryGlobeApp';
 import './poetry-globe.css';
+import './poetry-globe-pro.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
